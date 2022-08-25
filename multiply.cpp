3fa3da6f6feb09c80b35c12af06466376a5c1535
Abhay Kumar Sahu <aks.abhaykumarsahu@gmail.com>
2022-08-25 17:10:54 +0530

    multiplication added

---
+++ multiply.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"enter two numbers to multiply"<<endl;
+    cin>>a>>b;
+    c=a*b;
+    cout<<"the value is"<<c;
+    return 0;
+}
